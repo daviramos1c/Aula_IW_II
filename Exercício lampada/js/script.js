c772e7217c92a8899e11e8471b2bbbbeaf58240a
daviramos1c <daviramos1703@gmail.com>
2024-03-11 20:50:25 -0300

    clonagem

--- Exercício lampada/js/script.js
+++ Exercício lampada/js/script.js
@@ -1,32 +1,63 @@
-const ligar = document.getElementById('botao')
+const botao = document.getElementById('botao')
 const lampada = document.getElementById('lamp')
 const restaurar = document.getElementById('restaurar')
 
-function estlamp(){
+function vrlamp(){ //verifica se ela está quebrada
 
     return lampada.src.indexOf('quebrada') > -1
-    //retorna verdadeiro ou falso
 
 }
 
-function mdlamp(){
+function lampbk(){ //quebra a lampada
 
-    return lampada.src.indexOf('ligada') > -1
-    //retorna verdadeiro ou falso
-
+    lampada.src = "img/quebrada.jpg" 
+    
 }
 
-function 
+lampada.addEventListener('dblclick', lampbk)
 
-function lampbk(){
-    lampada.src = "img/quebrada.jpg"
-}
-lampada.addEventListener('dblclick',lampbk)
+function lamprt(){ //restaura a lampada
 
-function lamprt(){
-
-    if (estlamp()){
-        lampada.src = "img/desligada.jpg"
+    if (vrlamp()){
+        lampada.src = "img/desligada.jpg" 
     }
 
 }
+
+restaurar.addEventListener('click', lamprt)
+
+function lamplig(){ //liga a lampada
+    if (!vrlamp()){
+        lampada.src = "img/ligada.jpg"
+    }
+}
+
+lampada.addEventListener('mouseover', lamplig)
+
+function lampdls(){ //desliga a lampada
+    if (!vrlamp()){
+        lampada.src = "img/desligada.jpg"
+    }
+}
+
+lampada.addEventListener('mouseleave', lampdls)
+
+function estlamp(){
+
+    return lampada.src.indexOf('desligada') > -1
+
+}
+
+function botaofunc(){
+    if (!vrlamp()){
+        if (estlamp()){
+            lampada.src = "img/ligada.jpg"
+        }    
+        else{
+            lampada.src = "img/desligada.jpg"
+        }
+    }
+
+}
+
+botao.addEventListener('click', botaofunc)
