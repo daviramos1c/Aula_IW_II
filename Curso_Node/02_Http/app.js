1de7aa4aa7f107ac8a55a159a2f90f6611950622
Davi Antonio Ramos Araujo <124791732+daviramos1c@users.noreply.github.com>
2024-05-20 12:37:13 -0300

    Delete Curso_Node directory

--- Curso_Node/02_Http/app.js
+++
@@ -1,9 +0,0 @@
-//Selecionar o módulo http do node
-var http = require('http')
-
-//Abrir um sevidor http
-http.createServer(function(req, res){
-    res.end('<b>O dia que um dos drake perde pra spin vai virar mochilinha pra dar refletida')
-}).listen(8081)
-
-console.log('Servidor salvando...')
